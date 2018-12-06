e30c6bdb752961c935275d4d5bed3ea8ee6d916f
profrobbins <profrobbins@users.noreply.github.com>
2018-12-05 20:17:58 -0600

    create admin role and admin page

---
+++ MentalHealthTracker.Web/MentalHealthTracker.Web/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MentalHealthTracker.Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            if(User.IsInRole("Admin"))
+            {
+                return View();
+            }
+            return RedirectToAction("Index","Entry");
+
+        }
+    }
+}
